6e7a481321a25b40cc1bce143a65e8a6791f33e8
akhil-s-kumar <akhilskumar120@gmail.com>
2024-12-07 16:02:54 +0530

    Next Config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      NEXT_PUBLIC_MY_SECRET_API_KEY: process.env.NEXT_PUBLIC_MY_SECRET_API_KEY,
+        NEXT_PUBLIC_GOOGLE_FORM_ID: process.env.NEXT_PUBLIC_GOOGLE_FORM_ID,
     },
   };
   
